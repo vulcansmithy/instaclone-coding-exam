c9d3cc1495876d333bb8d2ac159823b18a508600
VulcanSmithy <ulysses.legaspi@gmail.com>
2021-10-27 19:24:24 +0800

    Update the schema.rb.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_10_024754) do
+ActiveRecord::Schema.define(version: 2021_10_27_111915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,16 @@ ActiveRecord::Schema.define(version: 2021_03_10_024754) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["photo_id"], name: "index_comments_on_photo_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "friendship_requests", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "requestor_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "aasm_state"
+    t.index ["requestor_id"], name: "index_friendship_requests_on_requestor_id"
+    t.index ["user_id"], name: "index_friendship_requests_on_user_id"
   end
 
   create_table "photos", force: :cascade do |t|

cfb7a14ffe3eb96f56e357c6dd2b280fdead7445
VulcanSmithy <ulysses.legaspi@gmail.com>
2021-10-27 19:23:11 +0800

    Fix missing foreign key.

--- db/migrate/20211027111336_create_friendship_requests.rb
+++ db/migrate/20211027111336_create_friendship_requests.rb
@@ -2,6 +2,7 @@ class CreateFriendshipRequests < ActiveRecord::Migration[6.1]
   def change
     create_table :friendship_requests do |t|
       t.references :user
+      t.references :requestor
         
       t.timestamps
     end

330ae507ff4d2885689f99db7b5149b1df8e35c7
VulcanSmithy <ulysses.legaspi@gmail.com>
2021-10-27 20:01:18 +0800

    Added rails routes for accepting and declining Friends Request.

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,9 @@ Rails.application.routes.draw do
   ##
   post "profiles/:username/add_friend",      to: "profiles#add_friend",      as: :profile_add_friend
   get  "profiles/:username/friend_requests", to: "profiles#friend_requests", as: :profile_friend_requests 
+
+  post "profiles/friend_request_accept/:friendship_request_id",  to: "profiles#friend_request_accept",  as: :friend_request_accept
+  post "profiles/friend_request_decline/:friendship_request_id", to: "profiles#friend_request_decline", as: :friend_request_decline
   ##
 
   resources :photos, only: [:index, :show, :new, :create, :destroy]

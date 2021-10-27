098e47cb03b640ef3fe2d8dcb02e6be211fc6616
VulcanSmithy <ulysses.legaspi@gmail.com>
2021-10-27 19:18:34 +0800

    Added another association representing the requestor.

--- app/models/friendship_request.rb
+++ app/models/friendship_request.rb
@@ -1,5 +1,6 @@
 class FriendshipRequest < ApplicationRecord
 
   belongs_to :user
+  belongs_to :requestor, class_name: "User", foreign_key: "requestor_id"
   
 end
--- db/migrate/20211027111336_create_friendship_requests.rb
+++ db/migrate/20211027111336_create_friendship_requests.rb
@@ -1,7 +1,8 @@
 class CreateFriendshipRequests < ActiveRecord::Migration[6.1]
   def change
     create_table :friendship_requests do |t|
-      t.references :user  
+      t.references :user
+        
       t.timestamps
     end
   end

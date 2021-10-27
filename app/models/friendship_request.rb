5562b01d3b99a9a2c6bb1e71739f687d23f66323
VulcanSmithy <ulysses.legaspi@gmail.com>
2021-10-27 19:21:40 +0800

    Flesh out additional code for state machine.

--- app/models/friendship_request.rb
+++ app/models/friendship_request.rb
@@ -1,10 +1,31 @@
 class FriendshipRequest < ApplicationRecord
+  
   include AASM
-
-  aasm do
-  end
 
   belongs_to :user
   belongs_to :requestor, class_name: "User", foreign_key: "requestor_id"
+
+  aasm whiny_transitions: false do
+    state :requested, initial: true
+    state :accepted
+    state :declined
+
+    event :friendship_accepted do
+      transitions from: :requested, to: :accepted
+
+      after do
+        self.save!
+      end
+    end  
+
+    event :friendship_declined do
+      transitions from: :requested, to: :declined
+
+      after do
+        self.save!
+      end 
+    end  
+  end
+
   
 end
